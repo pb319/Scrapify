4c3daf79d43742a01db0698c7e54369dd0a6dda9
pb319 <pranaybiswas319@gmail.com>
2024-08-11 22:03:57 +0530

    python file updated

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def find_job():
+    from bs4 import BeautifulSoup
+    import requests
+
+    html_text =  requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=Python&txtKeywords=Python&txtLocation=").text
+    soup = BeautifulSoup(html_text,"lxml")
+
+    blocks = soup.find_all("li",class_="clearfix job-bx wht-shd-bx")
+    companies = [block.h3.text.strip() for block in blocks]
+    skills = [block.find("span",class_="srp-skills").text.strip().replace(" ","") for block in blocks]
+    posts = [block.find("span",class_="sim-posted").text.strip().replace(" ","") for block in blocks]
+
+    
     x = input("Skills that you don't possess").split(",")
     for i in range(len(companies)):
         error = 1
@@ -16,3 +28,9 @@ def find_job():
     Skill Requirement: {skills[i]}
     Posted: {posts[i]}
     ''')
+        
+
+if __name__ == "__main__":
+    while True:
+        find_job()
+
