c3e65aa12e383c7d6fe98ae632653d8ea896a2cb
pb319 <pranaybiswas319@gmail.com>
2024-08-12 05:58:11 +0530

    output.csv added

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-def find_job():
-    from bs4 import BeautifulSoup
-    import requests
+from bs4 import BeautifulSoup
+import requests
+import time 
 
+def find_job():
     html_text =  requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=Python&txtKeywords=Python&txtLocation=").text
     soup = BeautifulSoup(html_text,"lxml")
 
     blocks = soup.find_all("li",class_="clearfix job-bx wht-shd-bx")
     companies = [block.h3.text.strip() for block in blocks]
-    skills = [block.find("span",class_="srp-skills").text.strip().replace(" ","") for block in blocks]
+    skills = [block.find("span",class_="srp-skills").text.strip().replace(" ","").replace(",","_") for block in blocks]
     posts = [block.find("span",class_="sim-posted").text.strip().replace(" ","") for block in blocks]
+    more_infos =[block.h2.a["href"] for block in blocks] 
 
     
-    x = input("Skills that you don't possess").split(",")
+    x = input("Skills that you don't possess \n").split(",")
+
+    str1 ="Posted,Company Name,Skill Requirement,More Info\n"
+
     for i in range(len(companies)):
         error = 1
         if (posts[i] == "Postedfewdaysago"):
@@ -23,14 +28,23 @@ def find_job():
             continue
         if error == 0:
             continue
+        #Writing Outputs into text files
         print(f'''
-    Company Name: {companies[i]}
-    Skill Requirement: {skills[i]}
-    Posted: {posts[i]}
+Company Name: {companies[i]}
+Skill Requirement: {skills[i]}
+Posted: {posts[i]}
+More Info: {more_infos[i]}
     ''')
+       
+        str1 = str1 + (f"{posts[i]};{companies[i]};{skills[i]};{more_infos[i]} \n")
         
-
+    with open(f"output.csv","w") as f:
+        f.write(str1)
+        
 if __name__ == "__main__":
     while True:
         find_job()
+        time_wait = 10
+        print(f"Waiting time: {time_wait} Minutes")
+        time.sleep(time_wait*60)
 
